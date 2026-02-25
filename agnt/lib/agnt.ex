5fc003a6296ab91a2da80938773326dfe0f6cd1c
Lucas Lopes Paszinski <lucaspaszinski@gmail.com>
2026-02-25 18:23:41 -0300

    Solving the biggest Ai problem with agents

---
+++ agnt/lib/agnt.ex
@@ -0,0 +1,18 @@
+defmodule Agnt do
+  @moduledoc """
+  Documentation for `Agnt`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Agnt.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
